ab5330d1f9090f65b34fcc395c9a1faaf99ba898
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-12-28 02:04:18 +0700

    up

---
+++ EnvironmentalProtectionSurvey/CustomValidation/LessDateAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnvironmentalProtectionSurvey.CustomValidation
+{
+    public class LessDateAttribute : ValidationAttribute
+    {
+        public LessDateAttribute() : base("{0} Date should greater than past")
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime? date = value as DateTime?;
+            if (date == null)
+            {
+                return true; // Allow null values
+            }
+
+            if (date.Value < DateTime.Today)
+            {
+                ErrorMessage = "Date should be greater than or equal to today.";
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- EnvironmentalProtectionSurvey/Models/Survey.cs
+++ EnvironmentalProtectionSurvey/Models/Survey.cs
@@ -1,22 +1,24 @@
-﻿using System;
+﻿using EnvironmentalProtectionSurvey.CustomValidation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EnvironmentalProtectionSurvey.Models;
 
 public partial class Survey
 {
     public int Id { get; set; }
-
+    [Required]
     public string? Title { get; set; }
 
     public string? UserType { get; set; }
-
+    [Required]
     public string? Form { get; set; }
 
     public int? UserPost { get; set; }
-
+    [LessDate]
     public DateTime? CreatedAt { get; set; }
-
+    [Required]
     public DateTime? EndAt { get; set; }
 
     public bool? IsVisible { get; set; }
