09a3996da34df52ce4550c333ed92ca175f62348
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-12-28 23:54:41 +0700

    up

--- EnvironmentalProtectionSurvey/Controllers/HomeController.cs
+++ EnvironmentalProtectionSurvey/Controllers/HomeController.cs
@@ -99,6 +99,93 @@ namespace EnvironmentalProtectionSurvey.Controllers
             return View(viewModel);
         }
 
+        public IActionResult SurveyBoard()
+        {
+            //lay ra user đăng nhâp 
+            DateTime currentDate = DateTime.Now;
+
+            var username = HttpContext.Session.GetString("username");
+            var user = _context.Users.FirstOrDefault(u => u.UserName == username);
+            var news = _context.News.ToList();
+            // Lọc những cuộc thi còn hạn
+            var contests = _context.Contests
+                .Where(c => c.StartTime <= currentDate && c.EndTime >= currentDate)
+                .OrderByDescending(c => c.Id)
+                .ToList();
+            List<Survey> survey;
+            if (user == null)
+            {
+
+                survey = _context.Surveys
+                    .Where(s => s.IsVisible == null && s.CreatedAt <= currentDate && (s.EndAt == null || s.EndAt >= currentDate))
+                    .OrderByDescending(s => s.Id)
+                    .Include(s => s.Questions)
+                        .ThenInclude(q => q.Options)
+                    .ToList();
+            }
+            else if (user.Role == "Admin")
+            {
+
+                survey = _context.Surveys
+                    .Where(s => s.IsVisible == null && s.CreatedAt <= currentDate && (s.EndAt == null || s.EndAt >= currentDate))
+                    .OrderByDescending(s => s.Id)
+                    .Include(s => s.Questions)
+                    .ThenInclude(q => q.Options)
+                    .ToList();
+            }
+            else
+            {
+
+                survey = _context.Surveys
+                    .Where(s => s.IsVisible == null && s.UserType == user!.Role || s.UserType == "All" && s.CreatedAt <= currentDate && (s.EndAt == null || s.EndAt >= currentDate))
+                    .OrderByDescending(s => s.Id)
+                    .Include(s => s.Questions)
+                        .ThenInclude(q => q.Options)
+                    .ToList();
+
+            }
+            List<int> studentCounts = new List<int>();
+            List<int> teacherCounts = new List<int>();
+
+            foreach (var s in survey)
+            {
+                var studentCount = _context.Users
+                    .Join(_context.FilledSurveys,
+                        user => user.Id,
+                        filledSurvey => filledSurvey.UserId,
+                        (user, filledSurvey) => new { User = user, FilledSurvey = filledSurvey })
+                    .Where(joinResult => joinResult.User.Role == "Student" && joinResult.FilledSurvey.SurveyId == s.Id)
+                    .GroupBy(joinResult => joinResult.User.Id)
+                    .Count();
+
+                var teacherCount = _context.Users
+                    .Join(_context.FilledSurveys,
+                        user => user.Id,
+                        filledSurvey => filledSurvey.UserId,
+                        (user, filledSurvey) => new { User = user, FilledSurvey = filledSurvey })
+                    .Where(joinResult => joinResult.User.Role == "Teacher" && joinResult.FilledSurvey.SurveyId == s.Id)
+                    .GroupBy(joinResult => joinResult.User.Id)
+                    .Count();
+
+                // Add the counts to the lists
+                studentCounts.Add(studentCount);
+                teacherCounts.Add(teacherCount);
+            }
+
+            // Pass the lists to the view using ViewBag
+            ViewBag.StudentParticipantCounts = studentCounts;
+            ViewBag.TeacherParticipantCounts = teacherCounts;
+
+            // Create the view model
+            var viewModel = new NewsSurveyViewModel
+            {
+                NewsList = news,
+                SurveyList = survey,
+                ContestList = contests
+            };
+            return View(viewModel);
+        }
+
         public IActionResult Supports()
         {
             return View();
